cd982eb2dd177dc8e0c8c820434047492680f19c
Marko Justinek <marko.justinek@gmail.com>
2020-04-27 11:17:19 +1000

    iOS example
    - Test Target with failing tests for demo how to catch invalid API Client implementation,
    - Test Target with passing tests to demonstrate the Pact contract as the result of passing tests.

---
+++ Pact-iOS-Example/Pact-iOS-FAILING-ExampleTests/Pact_iOS_FAILING_ExampleTests.swift
@@ -0,0 +1,260 @@
+//
+//  Pact_iOS_FAILING_ExampleTests.swift
+//  Pact-iOS-FAILING-ExampleTests
+//
+//  Created by Marko Justinek on 27/4/20.
+//  Copyright © 2020 PACT Foundation. All rights reserved.
+//
+
+import XCTest
+import PactSwift
+
+@testable import Pact_iOS_Example
+
+class XCTestFailingExampleTests: XCTestCase {
+
+	var mockService = MockService(consumer: "test-consumer", provider: "test-provider")
+
+	override func tearDown() {
+		mockService.finalize {
+			switch $0 {
+			case .success: return
+			case .failure(let error):
+				XCTFail(error.description)
+			}
+		}
+		super.tearDown()
+	}
+
+	// Test that the makeRequest() method handles the request and response based on our expectations
+	func testGetsUsers() {
+		// Expectations
+		_ = mockService
+			.uponReceiving("A request for a list of users")
+			.given("users exist")
+			.withRequest(
+				method: .GET,
+				path: "/api/users"
+			)
+			.willRespondWith(
+				status: 200,
+				body: userDataResponse
+			)
+
+		let apiClient = RestManager()
+
+		// Testing the expectations by running our makeRequest() method,
+		// pointing it to the mockService we programmed it just above
+
+		// This following block tests our RestManager implementation...
+		mockService.run { [unowned self] completed in
+			guard let url = URL(string: "\(self.mockService.baseUrl)/api/users") else {
+				XCTFail("Failed to prepare url!")
+				return
+			}
+
+			// This is using our API Client implementation in the main target.
+			apiClient.makeRequest(toURL: url, withHttpMethod: .get) { results in
+				if let data = results.data {
+					let decoder = JSONDecoder()
+					decoder.keyDecodingStrategy = .convertFromSnakeCase
+					guard let userData = try? decoder.decode(UserData.self, from: data) else {
+						// There's something with the structure of data either on our end, or how we defined the body we are expecting...
+						// Either way, they don't match meaning there's something wrong with our decoder implementation (or model) or body defined in .willRespondWith(...)
+						XCTFail("Failed to decode UserData")
+						completed() // Notify MockService we're done with our test
+						return
+					}
+
+					// test the response from server is what we expected and it decodes into userData
+					XCTAssertEqual(userData.page, 1)
+					XCTAssertEqual(userData.data?.first?.firstName, "John")
+					XCTAssertEqual(userData.data?.first?.lastName, "Tester")
+				}
+				completed() // Notify MockService we're done with our test
+			}
+		}
+	}
+
+	func testGetsUsers_Fails_WhenRequestPathInvalid() {
+		_ = mockService
+			.uponReceiving("Second request for a list of users")
+			// These request descriptions must be unique per Pact contract between a consumer and provider.
+			// So that when validating on Provider side, or even just going through Pact Broker list of requests you can differentiate between them. Makes sense, doesn't it.
+			.given("users exist")
+			.withRequest(
+				method: .GET,
+				path: "/api/users" // this is what we promise we will call
+			)
+			.willRespondWith(
+				status: 200,
+				body: userDataResponse
+			)
+
+		let apiClient = RestManager()
+
+		// Testing the expectations by running our makeRequest() method,
+		// pointing it to the mockService we programmed it just above
+
+		// But! We're going to call invalid path: /user instead of /users!!!
+		// This test should fail, because MockService should tell us we're not doing what we promised we will
+		// Pact error reason: Missing request - Because we did not call /user 🤷‍♂️
+		mockService.run { [unowned self] completed in
+			guard let url = URL(string: "\(self.mockService.baseUrl)/api/user") else {
+				XCTFail("Failed to prepare url!")
+				return
+			}
+
+			apiClient.makeRequest(toURL: url, withHttpMethod: .get) { results in
+				// we could write expectations here, but this test demonstrates how MockService intercepts our bad code...
+				completed() // Notify MockService we're done with our test
+			}
+		}
+	}
+
+	func testGetsUsers_Fails_WhenNoExpectedRequestIsMade() {
+		_ = mockService
+			.uponReceiving("Third request for a list of users")
+			.given("users exist")
+			.withRequest(
+				method: .GET,
+				path: "/api/users"
+			)
+			.willRespondWith(
+				status: 200,
+				body: userDataResponse
+			)
+
+		mockService.run(timeout: 1) { completed in
+			/*
+			Some code that we assume would make the API call we defined in .withRequest()
+			Something like:
+			apiClient.makeRequest(toURL: url, withHttpMethod: .get) { _ in
+				// handling code here //
+			}
+			*/
+
+			// Let's assume we do call apiClient.makeRequest() but the task.resume() is not called in there...
+			// Or something else blocked our apiClient to make the actual call. So no API call is made.
+			// So for demo purpouse, let's not make the call here as we did in the above tests.
+			// This test should fail because Pact MockService should respond with error saying
+			// we didn't make the api call we promised we would:
+
+			completed() // Notify MockService we're done with our test
+		}
+	}
+
+	func testGetsUsers_Fails_WhenQueryParametersAreMissing() {
+		_ = mockService
+			.uponReceiving("Third request for a list of users")
+			.given("users exist")
+			.withRequest(
+				method: .POST,
+				path: "/api/users",
+				query: [
+					"states": ["VIC","NSW","TAS"],
+					"page": ["2"]
+				],
+				body: ["name": "John"]
+			)
+			.willRespondWith(
+				status: 201,
+				body: userDataResponse
+			)
+
+		let apiClient = RestManager()
+
+		mockService.run(timeout: 1) { completed in
+			guard let url = URL(string: "\(self.mockService.baseUrl)/api/users?states=TAS") else {
+				XCTFail("Failed to prepare url!")
+				return
+			}
+
+			// This is using our API Client implementation in the main target.
+			apiClient.requestHttpHeaders.add(value: "application/json", forKey: "Content-Type")
+			apiClient.httpBodyParameters.add(value: "John", forKey: "name")
+
+			apiClient.makeRequest(toURL: url, withHttpMethod: .post) { results in
+				completed() // Notify MockService we're done with our test
+			}
+		}
+	}
+
+	func testGetsUsers_Fails_WhenHeadersMissmatch() {
+		_ = mockService
+			.uponReceiving("Fourth request for a list of users")
+			.given("users exist")
+			.withRequest(
+				method: .POST,
+				path: "/api/users",
+				headers: [
+					"Xample": "Demo",
+					"Content-Type": "application/json"
+				],
+				body: [
+					"name": "John"
+				]
+			)
+			.willRespondWith(
+				status: 201,
+				body: userDataResponse
+			)
+
+		let apiClient = RestManager()
+
+		mockService.run(timeout: 1) { completed in
+			guard let url = URL(string: "\(self.mockService.baseUrl)/api/users") else {
+				XCTFail("Failed to prepare url!")
+				return
+			}
+
+			// This is using our API Client implementation in the main target.
+			apiClient.requestHttpHeaders.add(value: "xample", forKey: "demo-for-you") // We promised we will send "Xample": "Demo"
+			apiClient.requestHttpHeaders.add(value: "application/json", forKey: "Content-Type")
+			apiClient.httpBodyParameters.add(value: "John", forKey: "name")
+
+			apiClient.makeRequest(toURL: url, withHttpMethod: .post) { results in
+				// No need to handle response in this example, as the request fails because the header we're sending
+				// does not match the header we promised we will send... We promised "Xample": "Demo", but sent "xample": "demo-for-you"
+				completed() // Notify MockService we're done with our test
+			}
+		}
+	}
+
+	func testGetsUsers_Fails_WhenBodyMissmatch() {
+		// Expectations
+		_ = mockService
+			.uponReceiving("Fifth request for a list of users")
+			.given("users exist")
+			.withRequest(
+				method: .POST,
+				path: "/api/users",
+				body: [
+					"name": "Julia"
+				]
+			)
+			.willRespondWith(
+				status: 201
+			)
+
+		let apiClient = RestManager()
+
+		mockService.run(timeout: 1) { completed in
+			guard let url = URL(string: "\(self.mockService.baseUrl)/api/users") else {
+				XCTFail("Failed to prepare url!")
+				return
+			}
+
+			// This is using our API Client implementation in the main target.
+			apiClient.requestHttpHeaders.add(value: "application/json", forKey: "Content-Type")
+			//  apiClient.httpBodyParameters.add(value: "John", forKey: "name") // We promised we will send "Julia"
+
+			apiClient.makeRequest(toURL: url, withHttpMethod: .post) { results in
+				//  No need to handle response in this example, as the request fails because the body we're sending
+				//  does not match the body we promised we will send... We promised "name": "Julia"
+				completed() // Notify MockService we're done with our test
+			}
+		}
+	}
+
+}
---
+++ Pact-iOS-Example/README.md
@@ -0,0 +1,68 @@
+# iOS Example
+
+## Disclaimer
+
+The code in this repository does not represent good programming practices!
+
+This repository is used solely to demonstrate the minimal effort required to demonstrate how to set up and use [`PactSwift`](https://github.com/surpher/pact-swift) framework.
+
+----
+
+## Prepare dependencies
+
+Add [`PactSwift`](https://github.com/surpher/pact-swift) to your `Cartfile` :
+
+	github "surpher/pact-swift" "master"
+
+_set version or branch - this one uses branch due to framework still being actively developed without a release version_
+
+Build [`PactSwift`](https://github.com/surpher/pact-swift) as dependency in your terminal:
+
+	carthage update --platform ios --no-use-binaries --cache-builds
+
+## Xcode setup
+
+[`PactSwift`](https://github.com/surpher/pact-swift) is a testing framework! Do not embed this framework into your deployable app bundle!
+
+Your app target, ideally, should not know about [`PactSwift`](https://github.com/surpher/pact-swift) at all!
+
+### Setup Build Phases
+
+`Test Target > Build Settings > Link Binary With Libraries > Add Other > Add Files...`  
+Find your Carthage folder, `./Build/iOS/` and select `PactSwift.framework` to link it:
+
+![link binary with libraries](res/01_link_binary_with_libraries.png)
+
+... Or drag and drop the framework into the Build Phase.
+
+### Setup Framework Build Settings
+
+1. In `Test Target > Build Settings` add `$(PROJECT_DIR)/Carthage/Build/iOS` (non-recursive) to `Framework Search Paths` configuration key.
+
+![set framework search paths](res/02_framework_search_paths.png)
+
+2. In `Test Target > Build Settings` Add `$(FRAMEWORK_SEARCH_PATHS)` to `Linking > Runpath Search Paths`
+
+![set runpath search paths](res/03_runpath_search_paths.png)
+
+### Set destination dir
+
+Edit scheme and add `PACT_DIR` environment variable with `dir` where you want your Pact contracts to be written to:
+
+![set destination dir](res/04_destination_dir.png)
+
+**NOTE:** If this environment variable is not set, the default dir [`PactSwift`](https://github.com/surpher/pact-swift) writes the contract to is `/tmp/pacts`.
+
+## Run tests
+
+1. Target `Pact-iOS-FAILING-ExampleTest` will fail with 4 tests and show the benefits of testing your "real" API Client implementation.
+
+2. Target `Pact-iOS-ExampleTests` will pass all tests and as a result a _[consumer_name]-[provider_name].json_ (eg: `passing-app-test-provider.json`) Pact Contract file is generated in `$(PROJECT_DIR)/tmp/pacts` folder.
+
+## Notes
+
+Repeat for any test target that does Pact testing.
+
+## References 
+
+[PACT Foundation](https://docs.pact.io)
