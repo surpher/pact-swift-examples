b643edee04154e40ace3ba57c05d1473b5c13614
Marko Justinek <marko.justinek@gmail.com>
2020-09-19 21:16:10 +1000

    Update tests to demo Example Generators

--- Pact-macOS-Example/Pact-macOS-Example-Tests/Pact_macOS_Example_Tests.swift
+++ Pact-macOS-Example/Pact-macOS-Example-Tests/Pact_macOS_Example_Tests.swift
@@ -247,6 +247,91 @@ class Pact_macOS_Example_CarthageTests: XCTestCase {
 		}
 	}
 
+	func testCreateUser_UsingExampleGenerator() {
+		// Expectations
+		_ = mockService
+			.uponReceiving("A request to create a user (using example generators)")
+			.given(ProviderState(description: "user does not exist", params: ["name" : "Julia"]))
+			.withRequest( // this is what we promise our apiClient will call
+				method: .POST,
+				path: "/api/users/add/verbose",
+				body: [
+					"name": Matcher.SomethingLike("Julia") // We only say we care about the key name ("name") and the type of value (a `String` should be expected)
+				]
+			)
+			.willRespondWith(
+				status: 201,
+				body: [
+					"page": Matcher.SomethingLike(1),
+					"per_page": Matcher.SomethingLike(25),
+					"total": Matcher.SomethingLike(1234),
+					"total_pages": Matcher.SomethingLike(493),
+					"data": [
+						[
+							"id": Matcher.SomethingLike(1),
+							"uuid": ExampleGenerator.RandomUUID(),
+							"first_name": Matcher.SomethingLike("John"),
+							"last_name": Matcher.SomethingLike("Tester"),
+							"joined": ExampleGenerator.RandomDate(format: "dd.MM.yyyy"),
+							// and we don't care about the avatar 🤷‍♂️
+						]
+					]
+				]
+			)
+
+		let apiClient = RestManager()
+
+		mockService.run(waitFor: 1) { completed in
+			guard let url = URL(string: "\(self.mockService.baseUrl)/api/users/add/verbose") else {
+				XCTFail("Failed to prepare url!")
+				return
+			}
+
+			// This is using our API Client implementation in the main target.
+			apiClient.requestHttpHeaders.add(value: "application/json", forKey: "Content-Type")
+			apiClient.httpBodyParameters.add(value: "Someones Name", forKey: "name") // We only promised we will send a `String`, so we're golden
+
+			apiClient.makeRequest(toURL: url, withHttpMethod: .post) { results in
+				if let data = results.data {
+					let decoder = JSONDecoder()
+					decoder.keyDecodingStrategy = .convertFromSnakeCase
+					guard let userData = try? decoder.decode(UserData.self, from: data) else {
+						// There's something with the structure of data either on our end, or how we defined the body we are expecting...
+						// Either way, they don't match meaning there's something wrong with our decoder implementation (or model) or body defined in .willRespondWith(...)
+						XCTFail("Failed to decode UserData")
+						completed() // Notify MockService we're done with our test
+						return
+					}
+
+					// test the response from server is what we expected and it decodes into userData
+					do {
+						// Assert the returned random Int is between 0 and 100
+						let resultPage = try XCTUnwrap(userData.page)
+						XCTAssertTrue((0...100).contains(resultPage))
+
+						// Assert the returned random string is a valid UUID
+						// MockServer might return a "simple" UUID (lowercased and
+						let resultUUID = try XCTUnwrap(userData.data?.first?.uuid)
+						if resultUUID.contains("-") {
+							XCTAssertNotNil(UUID(uuidString: resultUUID.uppercased()))
+						} else {
+							XCTAssertNotNil(resultUUID.uuid)
+						}
+
+						// Assert the returned random string is valid date with the specific format we expect it in
+						let resultDate = try XCTUnwrap(userData.data?.first?.joined)
+						let dateFormatter = DateFormatter()
+						dateFormatter.dateFormat = "dd.MM.yyyy"
+						XCTAssertNotNil(dateFormatter.date(from: resultDate))
+					} catch {
+						XCTFail("Failed to test example generators")
+					}
+				}
+				completed() // Notify MockService we're done with our test
+			}
+		}
+	}
+
 }
 
 extension Pact_macOS_Example_CarthageTests: URLSessionDelegate {
@@ -263,6 +348,21 @@ extension Pact_macOS_Example_CarthageTests: URLSessionDelegate {
 
 		let credential = URLCredential(trust: serverTrust)
 		completionHandler(.useCredential, credential)
+	}
+
+}
+
+private extension String {
+
+	var uuid: UUID? {
+		guard !self.contains("-") else {
+			return nil
+		}
+
+		var str: String = self
+		[8, 13, 18, 23].forEach { str.insert("-", at: str.index(str.startIndex, offsetBy: $0)) }
+
+		return UUID(uuidString: str.uppercased())
 	}
 
 }
