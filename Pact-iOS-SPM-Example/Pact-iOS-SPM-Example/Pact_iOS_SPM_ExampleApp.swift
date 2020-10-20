285cdd0ec5641cf04f505f807de999436c97e54a
Marko Justinek <marko.justinek@gmail.com>
2020-10-21 08:19:40 +1100

    Add Pact-iOS-SPM-Example project

---
+++ Pact-iOS-SPM-Example/Pact-iOS-SPM-Example/Pact_iOS_SPM_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pact_iOS_SPM_ExampleApp.swift
+//  Pact-iOS-SPM-Example
+//
+//  Created by Marko Justinek on 21/10/20.
+//
+
+import SwiftUI
+
+@main
+struct Pact_iOS_SPM_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pact-iOS-SPM-Example/Pact-iOS-SPM-ExampleTests/Pact_iOS_SPM_ExampleTests.swift
@@ -0,0 +1,33 @@
+//
+//  Pact_iOS_SPM_ExampleTests.swift
+//  Pact-iOS-SPM-ExampleTests
+//
+//  Created by Marko Justinek on 21/10/20.
+//
+
+import XCTest
+@testable import Pact_iOS_SPM_Example
+
+class Pact_iOS_SPM_ExampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
