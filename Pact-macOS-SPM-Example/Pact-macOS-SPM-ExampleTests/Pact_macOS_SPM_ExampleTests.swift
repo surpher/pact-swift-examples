26fd43a53edf3ed8ba8e5bd3ed6b4d8bd59ee192
Marko Justinek <marko.justinek@gmail.com>
2021-02-02 15:21:17 +1100

    Remove dead code

--- Pact-macOS-SPM-Example/Pact-macOS-SPM-ExampleTests/Pact_macOS_SPM_ExampleTests.swift
+++ Pact-macOS-SPM-Example/Pact-macOS-SPM-ExampleTests/Pact_macOS_SPM_ExampleTests.swift
@@ -345,24 +345,6 @@ class Pact_macOS_SPM_ExampleTests: XCTestCase {
 
 }
 
-extension Pact_macOS_SPM_ExampleTests: URLSessionDelegate {
-
-	func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-		guard
-			challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust,
-			(challenge.protectionSpace.host.contains("0.0.0.0") || challenge.protectionSpace.host.contains("localhost")),
-			let serverTrust = challenge.protectionSpace.serverTrust
-		else {
-			completionHandler(.performDefaultHandling, nil)
-			return
-		}
-
-		let credential = URLCredential(trust: serverTrust)
-		completionHandler(.useCredential, credential)
-	}
-
-}
-
 private extension String {
 
 	var uuid: UUID? {
